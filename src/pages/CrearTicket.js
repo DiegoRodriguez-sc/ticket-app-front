0c43e866b68659db13b5b1e61f15dd459862a5fd
DiegoRodriguez-sc <diego.rodriguez.sc@gmail.com>
2022-03-19 22:47:14 -0300

    crear ticket styles added

--- src/pages/CrearTicket.js
+++ src/pages/CrearTicket.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React from "react";
+import { Row, Col, Button, Typography } from "antd";
+import { DownloadOutlined } from "@ant-design/icons";
+
+const { Title, Text } = Typography;
 
 const CrearTicket = () => {
   return (
-    <div>
-       crear
-    </div>
+    <>
+      <Row>
+        <Col span={14} offset={6} align="center">
+          <Title level={2}>Presione el botón para un nuevo ticket</Title>
+          <Button type="primary" shape="round" icon={<DownloadOutlined />}>
+            Nuevo Ticket
+          </Button>
+        </Col>
+      </Row>
+      <Row style={{ marginTop: 100 }}>
+        <Col span={14} offset={6} align="center">
+          <Text level={2}>Su número</Text>
+          <br />
+          <Text type="success" style={{ fontSize: 55 }}>
+            55
+          </Text>
+        </Col>
+      </Row>
+    </>
   );
-}
+};
 
 export default CrearTicket;
