df5d7c56da76ee204854c66ca908836aa658e453
DiegoRodriguez-sc <diego.rodriguez.sc@gmail.com>
2022-03-21 11:45:59 -0300

    button salir config and information page escritorio

--- src/pages/Escritorio.js
+++ src/pages/Escritorio.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Navigate } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
 import { Button, Col, Divider, Row, Typography } from "antd";
 import { CloseCircleOutlined, RightOutlined } from "@ant-design/icons";
 import { getUsuarioStorage } from "../helper/getUsuarioStorage";
@@ -7,22 +7,28 @@ import { getUsuarioStorage } from "../helper/getUsuarioStorage";
 const { Title, Text } = Typography;
 
 const Escritorio = () => {
+  const navigate = useNavigate();
   const [usuario] = useState(getUsuarioStorage());
 
   if (!usuario.agente || !usuario.escritorio) {
     return <Navigate to={"/"} />;
   }
 
+  const handleSalir = () => {
+     localStorage.clear();
+    navigate("/");
+  }
+
   return (
     <>
       <Row>
         <Col span={20}>
-          <Title level={2}>Diego</Title>
+          <Title level={2}>{usuario.agente}</Title>
           <Text>Usted está trabajando en el escritorio: </Text>
-          <Text type="success">5</Text>
+          <Text type="success">{usuario.escritorio}</Text>
         </Col>
         <Col span={4} align="right">
-          <Button shape="round" type="danger">
+          <Button shape="round" type="danger" onClick={handleSalir}>
             <CloseCircleOutlined />
             Salir
           </Button>
