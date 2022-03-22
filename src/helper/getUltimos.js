ba23e8802927df70d0fb129359820596a0ad96ca
DiegoRodriguez-sc <diego.rodriguez.sc@gmail.com>
2022-03-22 17:25:20 -0300

    url change

--- src/helper/getUltimos.js
+++ src/helper/getUltimos.js
@@ -3,7 +3,7 @@
 export const getUltimos = async() => {
 
   // const url = `${process.env.REACT_APP_API_URL}/last`;
-  const url = `http://localhost:8080/last`;
+  const url = `https://ticket-app-socket.herokuapp.com/last`;
 
   const resp = await fetch(url);
   const data = await resp.json();
